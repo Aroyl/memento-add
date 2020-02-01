95dd4eaad51f694494be53745c0155236f5be6d8
Aroyl <4527071+Aroyl@users.noreply.github.com>
2020-02-01 11:40:03 +1000

    Update myfunctions.js

--- myfunctions.js
+++ myfunctions.js
@@ -1,9 +1,9 @@
 /* ----------------- Однострочники ------------------ */
-  var f = function (e) {return entry().field(e)}
-  var twoF = function (e) {return Math.round((e+0.00001)*100)/100}
-  var fe = function (p,e) {return p.field(e)}
-  var mTime = function (e) {return moment(e).format("HH:mm:ss")}
-  var mDate = function (e) {return moment(e).format("DD.MM.YYYY")}
+  var f = function (e) {return entry().field(e)};
+  var twoF = function (e) {return Math.round((e+0.00001)*100)/100};
+  var fe = function (p,e) {return p.field(e)};
+  var mTime = function (e) {return moment(e).format("HH:mm:ss")};
+  var mDate = function (e) {return moment(e).format("DD.MM.YYYY")};
 
 /* ---------------- Сложные функции ---------------- */
   var decode = function (text, charset){
@@ -13,5 +13,4 @@
       uint[i] = text.charCodeAt(i);
     }
     return decoder.decode(uint);
-  }
-};
+  };
