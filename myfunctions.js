4f7e1f364be7901e40ead7e5f79d7eba0810e8d1
Aroyl <4527071+Aroyl@users.noreply.github.com>
2020-02-29 23:52:05 +1000

    Update myfunctions.is
    Changed signature of "f" function to remove excess "ef" function. Changed mTime to timeIt, mDate to dateIt

--- myfunctions.js
+++ myfunctions.js
@@ -1,11 +1,14 @@
 /* ----------------- Однострочники ------------------ */
-  var f = function (e) {return entry().field(e)};
   var toFix = function (e) {return Math.round((e+0.00001)*100)/100};
-  var ef = function (p,e) {return p.field(e)};
-  var mTime = function (e) {return moment(e).format("HH:mm:ss")};
-  var mDate = function (e) {return moment(e).format("DD.MM.YYYY")};
+  var timeIt = function (e) {return moment(e).format("HH:mm:ss")};
+  var dateIt = function (e) {return moment(e).format("DD.MM.YYYY")};
 
 /* ---------------- Сложные функции ---------------- */
+  var f = function (p,e){
+    e = (typeof e !== 'undefined') ? e : entry();
+    return e.field(p);
+  };
+  
   var decode = function (text, charset){
     var decoder = new TextDecoder(charset);
     var uint = new Uint8Array(text.length);
